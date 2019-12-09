74a9788c77859964f6366563ea5613d2315c2a8d
GiannisVagionakis <57878676+GiannisVagionakis@users.noreply.github.com>
2019-12-09 15:31:45 +0200

    Add files via upload

--- Questionnaire.java
+++ Questionnaire.java
@@ -4,7 +4,7 @@ public class Questionnaire {
 public Questionnaire(Reservation reservation, int[] answers) {
 	
 	}
-Scanner sc = new Scanner(System.in);
+public static int[] answers=new int[10];
 public static char[] que = new char[10];
 que[0]="How did you find the hotel?";
 que[1]="How clean the hotel was?";
@@ -17,5 +17,13 @@ que[7]="Are you happy adout the price of the hotel";
 que[8]="Would you visit the hotel again?";
 que[9]="Would recommend the hotel to a friend or acquaintance";
 }
-
+}
+public static void run_que(char[]que) {
+	Scanner sc = new Scanner(System.in);
+	for(int x=0; x=9; x++) {
+	System.out.println(que[x]);
+	System.out.println("press a number from one to three and then enter:1=bad , 2=average , 3=good");
+	answers[x]=sc.nextInt();
+	}
+}
 }
